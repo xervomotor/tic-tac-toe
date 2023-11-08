d37d31ce925ebb9f230e053e62a37ecc2a0bf6b4
xtang999 <xiaoli05tang@gmail.com>
2023-11-08 16:29:45 +1100

    changed dom manipulation and event listeners to event delegation inside the module

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-// DOM manipulations
-const gameboardGrid = document.querySelector('#gameboard');
-const gameboardCell = document.querySelectorAll('.game-cell');
-
-gameboardCell.forEach((cell, index) => {
-    cell.addEventListener('click', () => {
-        Gameboard.setCell(john, index);
-    });
-})
-
 // Gameboard module (IIFE)
 const Gameboard = (() => {
     let gameboard = ['', '', '', '', '', '', '', '', ''];
+
+    const gameboardGrid = document.querySelector('#gameboard');
     
+    gameboardGrid.addEventListener('click', e => {
+        const cell = e.target;
+        const index = cell.dataset.index;
+        if (cell.className.includes('game-cell') && gameboard[index] === '') {
+            setCell(john, index);
+        }
+    });
+
     const getState = () => gameboard.slice();
     const setCell = (player, index) => {
         if (index >= gameboard.length) return;
         gameboard[index] = player.marker;
-        gameboardCell[index].textContent = player.marker;
+        document.querySelector(`[data-index="${index}"]`).textContent = player.marker;
     }
     const reset = () => {
         gameboard = ['', '', '', '', '', '', '', '', ''];
-        gameboardCell.forEach(cell => {
+        document.querySelectorAll('.game-cell').forEach(cell => {
             cell.textContent = '';
         })
     }
     return { setCell, reset, getState };
 })();
 
+// Factory function
 function createPlayer (name, marker) {
     return { name, marker };
 }
